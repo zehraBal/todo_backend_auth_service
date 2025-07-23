dcccec08ba6529af10db0ae6440aca95ae396f8d
zehraBal <fz.bal00@gmail.com>
2025-07-24 00:00:44 +0300

    feat(auth): add auth service core logic - Implement IAuthService interface - Add AuthService with register/login logic - Integrate UserServiceClient and JwtService

---
+++ src/main/java/com/bal/auth_service/service/IAuthService.java
@@ -0,0 +1,13 @@
+package com.bal.auth_service.service;
+
+import com.bal.auth_service.dto.LoginRequestDTO;
+import com.bal.auth_service.dto.LoginResponseDTO;
+import com.bal.auth_service.dto.SignUpRequestDTO;
+import com.bal.auth_service.dto.SignUpResponseDTO;
+
+
+public interface IAuthService {
+
+    SignUpResponseDTO register(SignUpRequestDTO dto);
+    LoginResponseDTO login(LoginRequestDTO dto);
+}
