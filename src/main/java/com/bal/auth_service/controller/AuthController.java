ea8155497f05e898a182204a569f77cf2956ea6e
zehraBal <fz.bal00@gmail.com>
2025-07-24 00:02:33 +0300

    feat(api): add auth controller - Implement AuthController with REST endpoints - Expose /api/auth/register and /api/auth/login

---
+++ src/main/java/com/bal/auth_service/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.bal.auth_service.controller;
+
+import com.bal.auth_service.dto.LoginRequestDTO;
+import com.bal.auth_service.dto.LoginResponseDTO;
+import com.bal.auth_service.dto.SignUpRequestDTO;
+import com.bal.auth_service.dto.SignUpResponseDTO;
+import com.bal.auth_service.service.IAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final IAuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<SignUpResponseDTO> register(@RequestBody SignUpRequestDTO signUpRequest) {
+        SignUpResponseDTO response = authService.register(signUpRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+        LoginResponseDTO response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
+}
